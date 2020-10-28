3a98e158e52ae456d41db3bf44f744e0c80ea952
nelly.skinner <daniellaskinner120@gmail.com>
2020-10-28 19:48:18 +0000

    created first python script omelette calculator

---
+++ omeletteCalculator.py
@@ -0,0 +1,16 @@
+import math
+
+numberOfEggsInABox = 6
+numberOfEggBoxes = 2
+eggsOmeletteNeeds = 4
+
+totalOmelettesMakeable = math.floor((numberOfEggsInABox * numberOfEggBoxes) / eggsOmeletteNeeds)
+
+if totalOmelettesMakeable == 1 and numberOfEggBoxes == 1:
+    output = "You can make {} omelette with {} box of eggs.".format(totalOmelettesMakeable, numberOfEggBoxes)
+elif totalOmelettesMakeable > 1 and numberOfEggBoxes == 1:
+    output = "You can make {} omelettes with {} box of eggs.".format(totalOmelettesMakeable, numberOfEggBoxes)
+else:
+    output = "You can make {} omelettes with {} boxes of eggs.".format(totalOmelettesMakeable, numberOfEggBoxes)
+
+print(output)
