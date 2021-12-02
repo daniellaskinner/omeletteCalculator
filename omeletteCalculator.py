3271822ee968298a56344a884d9fd5efc5795f0e
nelly.skinner <daniellaskinner120@gmail.com>
2021-12-02 12:20:44 +0000

    Refactor condition

--- omeletteCalculator.py
+++ omeletteCalculator.py
@@ -4,13 +4,16 @@ numberOfEggsInABox = 6
 numberOfEggBoxes = int(input('How many egg boxes you got?'))
 eggsOmeletteNeeds = 4
 
-totalOmelettesMakeable = math.floor((numberOfEggsInABox * numberOfEggBoxes) / eggsOmeletteNeeds)
+def calculateHowManyOmelettesICanMake():
+    totalOmelettesMakeable = math.floor((numberOfEggsInABox * numberOfEggBoxes) / eggsOmeletteNeeds)
 
-if totalOmelettesMakeable == 1 and numberOfEggBoxes == 1:
-    output = "You can make {} omelette with {} box of eggs.".format(totalOmelettesMakeable, numberOfEggBoxes)
-elif totalOmelettesMakeable > 1 and numberOfEggBoxes == 1:
-    output = "You can make {} omelettes with {} box of eggs.".format(totalOmelettesMakeable, numberOfEggBoxes)
-else:
-    output = "You can make {} omelettes with {} boxes of eggs.".format(totalOmelettesMakeable, numberOfEggBoxes)
+    if totalOmelettesMakeable == 1 and numberOfEggBoxes == 1:
+        output = "You can make {} omelette with {} box of eggs.".format(totalOmelettesMakeable, numberOfEggBoxes)
+    elif totalOmelettesMakeable > 1 and numberOfEggBoxes == 1:
+        output = "You can make {} omelettes with {} box of eggs.".format(totalOmelettesMakeable, numberOfEggBoxes)
+    else:
+        output = "You can make {} omelettes with {} boxes of eggs.".format(totalOmelettesMakeable, numberOfEggBoxes)
+
+    return totalOmelettesMakeable
 
 print(output)
